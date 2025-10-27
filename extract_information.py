9b0d9f0639349fd8a0ef4c9fbb76ab25c5a3cbe5
Bikash-Giri <bikashgiri17@gmail.com>
2025-10-27 10:40:56 +0100

    Extract image and textfile path name from csv image file name

--- extract_information.py
+++ extract_information.py
@@ -1,6 +1,7 @@
 from ast import literal_eval
 import os
 from pathlib import Path
+from pydoc import text
 from re import split
 
 from numpy.ma import count
@@ -8,49 +9,71 @@ import pandas as pd
 
 from image_extraction_helper import draw_bounding_box
 
-folder_path = "/Users/bikashgiri/Downloads/annotations_for_students/miceid_20251003144839"
+FOLDER_PATH = "/Users/bikashgiri/Downloads/annotations_for_students/miceid_20251003144839"
+csv_file = "output.csv"
+csv_file = []
 
-
+def extract_label_from_textfile(file):
+     with open(file, "r") as f:
+        text = f.read().split()
+        #convert text to float since we have number in text file
+        label_in_number = list(map(float, text))
+        for number in label_in_number:
+            if number > 4 :
+                return number
+                print(number)
+    
+        return 0
+                # print(label_in_number)
 
 def extract_row_information(row):
     
     """
-    Loop through all files in a folder.
+        Extract row information
     
     Parameters:
-    - folder_path: Path to the folder containing files
+    - row: row of current loop 
     """
 
-    labelledFilePath = Path(folder_path)/"labels"
+    label = Path(FOLDER_PATH)/"labels"
+    patch_label = 0
 
     
     # Check if folder exists
-    if not os.path.exists(labelledFilePath):
-        print(f"Folder '{labelledFilePath}' does not exist!")
+    if not os.path.exists(label):
+        print(f"Folder '{label}' does not exist!")
         return
     
     # Get all files in the folder
-    files = os.listdir(labelledFilePath)
-    
-    for filename in files:
-         
-        # Get file extension
-        file_ext = os.path.splitext(filename)[1].lower()
-        
-        if file_ext == '.txt':
-           # TODO extract tail mark label from .txt file 
-            a= "asdf"
-            
-        elif file_ext == '.png':
-            imageName = Path(row.image_id).name 
+    files = os.listdir(label)
 
-            if imageName.endswith(filename):
-                image_path = Path(labelledFilePath)/ filename 
-                splittedTailInfo = row.tailmark_info.split()
+    imageName = fr"{row.image_id}".replace("\\","/")
+    print(imageName)
+
+
+
+    path_without_extension = Path(imageName).stem
+    path_suffix = Path(path_without_extension).name
+    print(path_suffix + "suffix_name")
+    file_with_txt = path_without_extension + ".txt"
+    file_with_image = path_without_extension + ".png"
+    textfile = Path(label) / file_with_txt
+
+    patch_label = extract_label_from_textfile(textfile)
+
+
+
+
+    image_path = Path(label)/ file_with_image 
+    print("===================")
+    print(image_path )
+                
+                
+    splittedTailInfo = row.tailmark_info.split()
             
-                if  "Empty DataFrame" in splittedTailInfo:
+    if  "Empty DataFrame" in splittedTailInfo:
                     print("not found")
-                else:
+    else:
                     try:
                         # TODO This extracts only one mouse tail infromation.Remove the slice method 
                         slicedTailCoordinate = splittedTailInfo[17:21]
@@ -65,19 +88,68 @@ def extract_row_information(row):
                                 coordinates = coordinate,
                                 color=(0, 255, 0),  # Green in BGR
                                 thickness=3,
-                                label="Tail-Patches"
+                                label="Tail-Patches",
+                                class_label= patch_label
                             )
                         else:
                             print(f"Not enough coordinate data: {slicedTailCoordinate}")
                     except (ValueError, IndexError) as e:
                         print(f"Error processing coordinates: {e}")
+
+
+    
+    
+    # for filename in files:
+    #     file = Path(label)/ filename 
+    #     # Get file extension
+    #     file_ext = os.path.splitext(filename)[1].lower()
+        
+    #     if file_ext == '.txt':
+    #         # file = Path(label)/ filename  
+    #         # patch_label = extract_label_from_textfile(file) 
+    #         print("")
             
-        else:
-            print(f"  → Other file type: {file_ext}")
+    #     elif file_ext == '.png':
+    #         imageName = Path(row.image_id).name 
+
+    #         if imageName.endswith(filename):
+    #             image_path = Path(label)/ filename 
+                
+                
+    #             splittedTailInfo = row.tailmark_info.split()
+            
+    #             if  "Empty DataFrame" in splittedTailInfo:
+    #                 print("not found")
+    #             else:
+    #                 try:
+    #                     # TODO This extracts only one mouse tail infromation.Remove the slice method 
+    #                     slicedTailCoordinate = splittedTailInfo[17:21]
+    #                     print(slicedTailCoordinate)
+                        
+    #                     # Check if we have enough elements and they can be converted to float
+    #                     if len(slicedTailCoordinate) >= 4:
+    #                         coordinate = list(map(float,slicedTailCoordinate))
+    #                         print(f"Coordinates: {coordinate}")
+    #                         draw_bounding_box(
+    #                             image_path= image_path,
+    #                             coordinates = coordinate,
+    #                             color=(0, 255, 0),  # Green in BGR
+    #                             thickness=3,
+    #                             label="Tail-Patches",
+    #                             class_label= patch_label
+    #                         )
+    #                     else:
+    #                         print(f"Not enough coordinate data: {slicedTailCoordinate}")
+    #                 except (ValueError, IndexError) as e:
+    #                     print(f"Error processing coordinates: {e}")
+                        
+
+    #     else:
+    #         print(f"  → Other file type: {file_ext}")
 
 def iterate_over_CSV():
 
-    csvPath = Path(folder_path) / "train.csv"
+    csvPath = Path(FOLDER_PATH) / "train.csv"
     df = pd.read_csv(csvPath)
     df = df.reset_index(drop=True)
 
